c9d335d9bdad46ed8a4e9858ce3e422369bfd230
James.Holland <james.holland@oit.edu>
2026-06-17 00:08:42 -0700

    Added collision-based fission. Added clear button.

--- pydemo.py
+++ pydemo.py
@@ -30,14 +30,14 @@ class Particle:
         self.y_speed = y_speed
         self.x = x
         self.y = y
-        
         # type
         start_val = 0
         stop_val = 20 # must not be zero
 
         picker = rand.randrange(start_val, stop_val, 1)
+        self.key = hash(x * y * x_speed * y_speed * picker) # identification
 
-        if picker < 10:
+        if picker < (start_val + stop_val) / 2:
             self.type = 'Proton'
         else:
             self.type = 'Neutron'
@@ -49,10 +49,17 @@ class Particle:
             self.color = 'SlateGray'
     
     def __update__(self):
-        self.x += self.x_speed
-        self.y += self.y_speed
-        self.x_speed -= 0.1
-        self.y_speed -= 0.1
+        xdirection = rand.randrange(1, 10)
+        ydirection = rand.randrange(1, 10)
+        if xdirection < 5:
+            self.x += self.x_speed
+        else:
+            self.x -= self.x_speed
+
+        if ydirection < 5:
+            self.y += self.y_speed
+        else:
+            self.y -= self.y_speed            
 
 ######################################################################################################################################
 #                                                      URANIUM                                                                       #
@@ -60,10 +67,10 @@ class Particle:
 # Def: Uranium is the objects being statically held in a grid-style format                                                           #
 #                                                                                                                                    #
 # Method(s):                                                                                                                         #
-#   __init__             : 4-arg ctor                      - defines uranium particle. location and radius                           #
-#   __irradiate__        : 0-arg method                    - creates 1-5 random particles                                            #
-#   __update_particles__ : 0-arg method                    - function to easily update the location of loose particles               #
-#   __collision__        : 2-arg method (X-Coord, Y-Coord) - Returns whether a target is touching a uranium particle                 #
+#   __init__             : 4-arg ctor   - defines uranium particle. location and radius                                              #
+#   __irradiate__        : 0-arg method - creates 1-5 random particles                                                               #
+#   __update_particles__ : 0-arg method - function to easily update the location of loose particles                                  #
+#   __clear_particles__  : 0-arg method - sets all particle lists to []                                                              #
 ######################################################################################################################################
 class Uranium:
     def __init__(self, rad, color, x, y):
@@ -76,27 +83,17 @@ class Uranium:
     def __irradiate__(self):
         MAX_PARTICLES = 5
         num_particles = rand.randrange(1, MAX_PARTICLES)
-        if len(self.particles) > 10:
-            self.particles = []
         for _ in range(num_particles):
             RADIUS = 2
-            xspeed, yspeed = (rand.randrange(1, 10), rand.randrange(1, 10))
+            xspeed, yspeed = (rand.randrange(-10, 10), rand.randrange(-10, 10))
             self.particles.append(Particle(RADIUS, xspeed, yspeed, self.x, self.y))
 
     def __update_particles__(self):
         for i in range(len(self.particles)):
             self.particles[i].__update__()
-    
-    def __collision__(self, x, y):
-        x_touching = False
-        y_touching = False
 
-        if x >= self.x - self.radius and x <= self.x + self.radius:
-            x_touching = True
-        if y >= self.y - self.radius and y <= self.y + self.radius:
-            y_touching = True
-
-        return x_touching and y_touching
+    def __clear_particles__(self):
+        self.particles = []
         
 ######################################################################################################################################
 #                                                         GRID                                                                       #
@@ -106,6 +103,8 @@ class Uranium:
 # Method(s):                                                                                                                         #
 #   __init__             : 2-arg ctor   - consisting of row/col format to create a simple rectangular grid of particles.             #
 #   __update_radiation__ : 0-arg method - function to easily update the location of loose particles                                  #
+#   __collision__        : 4-arg method - function to detect collisions and automatically cause chain-reaction                       #
+#   __clear_fission__    : 0-arg method - function to reset all fission (all particle lists set to [])                               #
 ######################################################################################################################################
 class Grid:
     def __init__(self, rows, cols):
@@ -139,13 +138,38 @@ class Grid:
             for col in range(self.columns):
                 self.particles[row][col].__update_particles__()
 
+    def __collision__(self, x = 0, y = 0, parentx = 0, parenty = 0):
+        for row in range(self.rows):
+            for col in range(self.columns):
+                x_touching = False
+                y_touching = False
+
+                if x >= parentx - self.particles[row][col].radius and x <= parentx + self.particles[row][col].radius:
+                    if y >= parenty - self.particles[row][col].radius and y <= parenty + self.particles[row][col].radius:
+                        continue
+
+                if x >= self.particles[row][col].x - self.particles[row][col].radius and x <= self.particles[row][col].x + self.particles[row][col].radius:
+                    x_touching = True
+                if y >= self.particles[row][col].y - self.particles[row][col].radius and y <= self.particles[row][col].y + self.particles[row][col].radius:
+                    y_touching = True
+                if x_touching and y_touching:
+                    self.particles[row][col].__irradiate__()
+                    return True
+        return False
+    
+    def __clear_fission__(self):
+        for row in range(self.rows):
+            for col in range(self.columns):
+                self.particles[row][col].__clear_particles__()
+
 ######################################################################################################################################
 #                                                     GAMEPLAY                                                                       #
 ######################################################################################################################################
-ROWS = 3
-COLS = 3
+ROWS = 7
+COLS = 7
 BUTTON_WIDTH, BUTTON_HEIGHT = (170, 64)
-BUTTON_X, BUTTON_Y = (SCREEN_WIDTH - BUTTON_WIDTH - 50, 10)
+BUTTON_RADIATE_X, BUTTON_RADIATE_Y = (SCREEN_WIDTH - BUTTON_WIDTH - 50, 10)
+BUTTON_CLEAR_X, BUTTON_CLEAR_Y = (SCREEN_WIDTH - BUTTON_WIDTH - 50, 84)
 grid = Grid(ROWS, COLS)
 
 while running:
@@ -154,11 +178,10 @@ while running:
             running = False
         elif event.type == pg.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = event.pos
-            if pg.Rect(BUTTON_X, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT).collidepoint(mouse_x, mouse_y):
-                for row in range(grid.rows):
-                    for col in range(grid.columns):
-                        pg.draw.circle(screen, grid.color, (grid.particles[row][col].x, grid.particles[row][col].y), grid.radius)
-                        grid.particles[row][col].__irradiate__() # spawn particles
+            if pg.Rect(BUTTON_RADIATE_X, BUTTON_RADIATE_Y, BUTTON_WIDTH, BUTTON_HEIGHT).collidepoint(mouse_x, mouse_y):
+                grid.particles[0][0].__irradiate__()
+            elif pg.Rect(BUTTON_CLEAR_X, BUTTON_CLEAR_Y, BUTTON_WIDTH, BUTTON_HEIGHT).collidepoint(mouse_x, mouse_y):
+                grid.__clear_fission__()
 
     screen.fill("white")
 
@@ -166,7 +189,6 @@ while running:
     for row in range(grid.rows):
         for col in range(grid.columns):
             pg.draw.circle(screen, grid.color, (grid.particles[row][col].x, grid.particles[row][col].y), grid.radius)
-            # grid.particles[row][col].__irradiate__() # spawn particles
 
             for p in range(len(grid.particles[row][col].particles)):
                 pg.draw.circle(screen, grid.particles[row][col].particles[p].color, 
@@ -180,17 +202,28 @@ while running:
                 pg.draw.circle(screen, grid.particles[row][col].particles[p].color, 
                                (grid.particles[row][col].particles[p].x, grid.particles[row][col].particles[p].y),
                                grid.radius - 5)
-                t.sleep(0.0001) # arbitrary sleep time to slow animation slightly
-    
+
+                # fission chain-reaction method
+                grid.__collision__(grid.particles[row][col].particles[p].x, 
+                                   grid.particles[row][col].particles[p].y, 
+                                   grid.particles[row][col].x, grid.particles[row][col].y)
+
+
     # Button to Radiate
-    pg.draw.rect(screen, 'IndianRed', (BUTTON_X, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT))
+    pg.draw.rect(screen, 'IndianRed', (BUTTON_RADIATE_X, BUTTON_RADIATE_Y, BUTTON_WIDTH, BUTTON_HEIGHT))
     if pg.font:
         font = pg.font.Font(None, 64)
         text = font.render("Radiate", True, (10, 10, 10))
-        textpos = text.get_rect(x=BUTTON_X + 2, y=BUTTON_Y + 10)
+        textpos = text.get_rect(x=BUTTON_RADIATE_X + 2, y=BUTTON_RADIATE_Y + 10)
         screen.blit(text, textpos)
 
-
+    # Button to Reset
+    pg.draw.rect(screen, 'Orange', (BUTTON_CLEAR_X, BUTTON_CLEAR_Y, BUTTON_WIDTH, BUTTON_HEIGHT))
+    if pg.font:
+        font = pg.font.Font(None, 64)
+        text = font.render("Clear", True, (10, 10, 10))
+        textpos = text.get_rect(x=BUTTON_CLEAR_X + 2, y=BUTTON_CLEAR_Y + 10)
+        screen.blit(text, textpos)
 
     pg.display.flip()
     clock.tick(60)
